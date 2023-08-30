a5c6ad181346eeebb5a1d2748da4e2ec23412792
Hans Hoegh-Omdal <hahomdal@gmail.com>
2023-08-31 01:17:18 +0200

    test update

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 resource "random_id" "server" {
   byte_length = 8
 }
-
 resource "random_id" "server2" {
   byte_length = 8
+  depends_on = [random_id.server]
+}
+resource "random_id" "server3" {
+  byte_length = 8
+  depends_on = [random_id.server]
 }
