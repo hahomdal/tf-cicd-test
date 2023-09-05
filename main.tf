eac2dde391ad04e1dca19192aeb352517209ee96
Hans Hoegh-Omdal <hahomdal@gmail.com>
2023-09-05 14:17:23 +0200

    fix fmt main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ resource "random_id" "server" {
 }
 resource "random_id" "server2" {
   byte_length = 8
-  depends_on = [random_id.server]
+  depends_on  = [random_id.server]
 }
 resource "random_id" "server3" {
   byte_length = 8
-  depends_on = [random_id.server]
+  depends_on  = [random_id.server]
 }
